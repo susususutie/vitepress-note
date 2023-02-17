356a37e7926b0bf455b34c102c0523845f16c694
sutie <sususu_sutie@qq.com>
2023-02-17 10:28:34 +0800

    init

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,6 @@
+// https://juejin.cn/post/7199265829955108901#heading-1
+// https://vitepress.vuejs.org/guide/theme-introduction#customizing-css
+import DefaultTheme from 'vitepress/theme'
+import './index.css'
+
+export default DefaultTheme
